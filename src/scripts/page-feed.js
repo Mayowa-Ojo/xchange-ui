6c5532913723d274368711b9d01db97370fda900
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-02 23:43:49 -0700

    feat(script): add page feed refresh functionality

--- src/scripts/page-feed.js
+++ src/scripts/page-feed.js
@@ -4,7 +4,15 @@ import CacheStorage from './cache.js'
 class PageFeed {
     constructor() {
         this.listElement = document.querySelector('.feed-container')
+
         this.populateFeed()
+        this.addEventListener()
+    }
+
+    addEventListener() {
+        const userInput = document.querySelector('#user-input')
+
+        userInput.addEventListener('keypress', this.refreshFeed.bind(this))
     }
 
     populateFeed() {
@@ -13,7 +21,7 @@ class PageFeed {
         console.log(cacheData)
 
         // loop through cache data and call generate function
-        for(let i = 0; i < cacheData.length; i++) {
+        for(let i = cacheData.length-1; i >= 0; i--) {
             this.generateFeedItem(cacheData[i])
             console.log(`Populating feed container...${i}`)
         }
@@ -22,7 +30,7 @@ class PageFeed {
     generateFeedItem(data) {
         const inner_html = `<p class="feed-title">currency</p>
             <div class="feed-content">
-                <img class="country-flag" src="./assets/svg/uk.svg" alt="USA flag">
+                <img class="country-flag" src="./assets/svg/eu.svg" alt="USA flag">
                 <p>${data.base} (${data.input})</p>
                 <i class="fas fa-long-arrow-alt-right"></i>
                 <img class="country-flag" src="./assets/svg/nigeria.svg" alt="USA flag">
@@ -33,6 +41,15 @@ class PageFeed {
         listItem.classList.add('feed')
         listItem.innerHTML = inner_html
         this.listElement.appendChild(listItem)
+    }
+
+    refreshFeed(e) {
+        if(e.which === 13) {
+            // remove previous feed list
+            this.listElement.innerHTML = ''
+            // add updated feed
+            this.populateFeed()
+        }
     }
 }
 
