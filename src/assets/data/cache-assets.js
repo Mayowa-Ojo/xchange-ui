ba12a541e9f19036be301a32992634d956866267
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-14 02:30:45 -0700

    refactor(script): move cache assets array to separate module

---
+++ src/assets/data/cache-assets.js
@@ -0,0 +1,16 @@
+const assets = [
+   "/",
+   "./index.html",
+   "offline.html",
+   "./styles/style.css",
+   "./scripts/main.js",
+   "./scripts/cache.js",
+   "./scripts/page-feed.js",
+   "./scripts/slideshow.js",
+   "./favicon.ico",
+   "./favicon-16x16.png",
+   "./favicon-32x32.png",
+   "./apple-touch-icon.png",
+   "./android-chrome-512x512.png",
+   "./android-chrome-192x192.png"
+]
